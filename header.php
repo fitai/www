9523acfe48938ccbbd0d3ee65037c7441f1a27bf
Patrick <patrick@tayklin.com>
2016-09-28 19:57:39 -0700

    SASS setup

--- header.php
+++ header.php
@@ -13,6 +13,9 @@ require("/var/www/html/auth.php");
 <link rel="stylesheet" href="/css/webfont.css" />
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript" src="/js/charts.js"></script>
 </head>
 <body>
 <div id="body-content" class="body-<?php echo strtolower($title); ?>" >
@@ -55,3 +58,4 @@ require("/var/www/html/auth.php");
 		<header>
 			<img id="logo" src="/images/fitai-logo-black.svg">
 		</header>
+		<div id="dynamic-content">
--- now/index.php
+++ now/index.php
@@ -3,6 +3,7 @@ $title = "Now";
 include("/var/www/html/header.php");
 ?>
 <h1><?php echo $title; ?></h1>
+<?php /*
 School: <?php echo get_athlete_school(); ?><br>
 Coach: <?php echo get_coach_name(); ?><br>
 Team: <?php echo get_athlete_team_name(); ?><br>
@@ -11,7 +12,42 @@ Collar: <select name="collarID">
 			<option value="1">1</value>
 		</select><br>
 Next Lift: <?php print_r(get_next_lift()); ?>
+*/ ?>
 
+<div id="connect_string"></div>
+<div id="data-container" class="data-container flexbox">
+	<div class="tab">
+		<span id="rep-count" class="count-number">0</span> <span class="count-text">reps</span>
+	</div>
+</div>
+<div class="flexbox charts-container">
+	<div id="chart_div" class="chart"></div>
+	<div id="chart_column" class="chart"></div>
+</div>
+<script>
+var connectDiv = document.getElementById("connect_string");
+var gauge = document.getElementById("chart_div");
+connectDiv.innerHTML="attempting to establish connection...<br>";
+
+connectDiv.innerHTML=location.port;
+
+var conn = new WebSocket('ws://52.204.229.101:8080');
+connectDiv.innerHTML="did something...<br>";
+conn.onopen = function(e) {
+    console.log("Connection established!");
+    connectDiv.innerHTML="Connection successful<br>";
+};
+
+conn.onmessage = function(e) {
+    console.log(e.data);
+    connectDiv.innerHTML=e.data;
+	var values = JSON.parse(e.data);
+	updateGauge(values.velocity);
+	updateColumn(values.power);
+	updateReps(values.repCount);
+};
+connectDiv.innerHTML="\nDone!";
+</script>
 <?php
 include('/var/www/html/footer.php');
 ?>
